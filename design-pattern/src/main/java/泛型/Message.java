cac5a411eccfee2990fc08c275be9cb51cd701c5
蒋勇 <jiangy@highzap.com>
2020-06-24 09:25:29 +0800

    add .

---
+++ design-pattern/src/main/java/泛型/Message.java
@@ -0,0 +1,27 @@
+package 泛型;
+
+/**
+ * <p>描述: [类型描述] </p>
+ * <p>创建时间: 2020/6/23 </p>
+ *
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+public class Message<T> {
+
+
+    T content;
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public void func() {
+
+    }
+
+}
