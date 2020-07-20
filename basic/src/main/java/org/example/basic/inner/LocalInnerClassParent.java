6e68035ef1412892ca6da4467c089ee5ee874c13
蒋勇 <jiangy@highzap.com>
2020-07-20 22:53:47 +0800

    增加局部内部类测试代码

--- basic/src/main/java/org/example/basic/inner/LocalInnerClassParent.java
+++ basic/src/main/java/org/example/basic/inner/LocalInnerClassParent.java
@@ -3,7 +3,12 @@ package org.example.basic.inner;
 public class LocalInnerClassParent {
     public Object build(boolean inner) {
         if (inner) {
+            /**
+             * 1. 局部内部类**访问父类成员无限制**。
+             * 2. 局部内部类**没有修饰符**。
+             */
             class LocalInnerClass {
+                public LocalInnerClass() {} // 显式声明无参构造函数
                 private int id;
                 private String name;
                 private String localInnerClassName;
