72ccd195ade8b287633de801e1b256cb426e8b3c
蒋勇 <jiangy@highzap.com>
2021-04-28 00:25:09 +0800

    update transation case

--- springboot/src/main/java/org/example/springboot/cases/transational/UserService.java
+++ springboot/src/main/java/org/example/springboot/cases/transational/UserService.java
@@ -1,5 +1,18 @@
 package org.example.springboot.cases.transational;
 
 public interface UserService {
+    /**
+     * 声明式事务案例
+     * @param user
+     * @return
+     */
     boolean addUser(User user);
+
+    /**
+     * 命令式事务案例
+     *
+     * @param user
+     * @return
+     */
+    User commandTransactionCase(User user);
 }
