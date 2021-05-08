9eddd4d6f6b4dba53666a61bbca0bc57f2d45e7c
蒋勇 <jiangy@highzap.com>
2021-05-08 22:17:13 +0800

    update spy case

---
+++ unit-test/src/main/java/org/example/unittest/cases/spy/SpyCase.java
@@ -0,0 +1,14 @@
+package org.example.unittest.cases.spy;
+
+/**
+ * <p>创建时间: 2021/4/29 </p>
+ *
+ * 当你使用spy时，真正的方法会被调用（除非某个方法被存根）。
+ *
+ * https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html#33
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+public class SpyCase {
+
+}
