5d9d6e89cff17de6659fcebbaff0b4d9987ff904
蒋勇 <jiangy@highzap.com>
2021-08-12 15:45:28 +0800

    fix:移除eclipse的文件

---
+++ basic/src/main/java/org/example/basic/enum_/BusinessType.java
@@ -0,0 +1,8 @@
+package org.example.basic.enum_;/**
+ * <p>创建时间: 2021/7/19 </p>
+ *
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+public enum BusinessType {
+}
