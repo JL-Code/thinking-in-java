9a893e25dd46ea24ae6020270c1f94bdedb04955
蒋勇 <jiangy@highzap.com>
2021-08-27 13:50:55 +0800

    feat:增加 elk 日志输出测试用例

---
+++ elk-demo/src/main/java/org/example/elk/ElkDemoApplication.java
@@ -0,0 +1,18 @@
+package org.example.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>创建时间: 2021/8/27 </p>
+ *
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+@SpringBootApplication
+public class ElkDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElkDemoApplication.class, args);
+    }
+}
+
