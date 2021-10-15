b50d17e5f659db9bd1a1c99c884c90e21ca13aaa
蒋勇 <jiangy@highzap.com>
2021-10-15 14:25:29 +0800

    feat:新增 Package Version 信息查看案例

---
+++ springboot/src/main/java/org/example/springboot/controller/ProjectVersionController.java
@@ -0,0 +1,41 @@
+package org.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>创建时间: 2021/10/15 </p>
+ *
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+@RestController
+public class ProjectVersionController {
+
+
+    @GetMapping("/project/version")
+    public Object listManifestInfo() {
+
+        /**
+         * https://docs.oracle.com/javase/7/docs/api/java/lang/Package.html
+         */
+        Package packageInfo = ProjectVersionController.class.getPackage();
+
+        /**
+         * {
+         * name: "org.example.springboot.controller",
+         * annotations: [ ],
+         * declaredAnnotations: [ ],
+         * sealed: false,
+         * specificationTitle: null,
+         * specificationVersion: null,
+         * specificationVendor: null,
+         * implementationTitle: "springboot",  // 打包后运行才可以看到（IDEA 运行时无）
+         * implementationVersion: "1.0-SNAPSHOT", // 打包后运行才可以看到（IDEA 运行时无）
+         * implementationVendor: null
+         * }
+         */
+
+        return packageInfo;
+    }
+}
